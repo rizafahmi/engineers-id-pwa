2a895a2b11773cf6f1d65d5b71cd4eb4899d7632
Riza Fahmi <rizafahmi@gmail.com>
2017-07-17 14:09:20 +0700

    Load dynamic data

---
+++ scripts/template.js
@@ -0,0 +1,22 @@
+define([], () => {
+  const generateCard = video => {
+    let template = document.querySelector('#card').innerHTML
+    template = template.replace('{{title}}', video.title)
+    template = template.replace('{{description}}', video.description)
+    template = template.replace('{{image}}', video.thumbnail)
+    return template
+  }
+  const appendData = videos => {
+    let videoHTML = ''
+    for (let i = 0; i < videos.length; i++) {
+      videoHTML += generateCard(videos[i])
+    }
+    document.querySelector('.cards').insertAdjacentHTML('beforeend', videoHTML)
+    document.querySelector('.spinner').style.display = 'none';
+    ('none')
+  }
+
+  return {
+    appendData
+  }
+})
--- scripts/videoService.js
+++ scripts/videoService.js
@@ -1,10 +1,10 @@
-define([], () => {
+define(['./template.js'], template => {
   const apiUrlPath = 'http://localhost:3000/'
   const apiUrlVideosPath = apiUrlPath + 'videos'
 
   const loadData = () => {
     fetch(apiUrlVideosPath).then(res => res.json()).then(data => {
-      console.log(data)
+      template.appendData(data)
     })
   }
 
