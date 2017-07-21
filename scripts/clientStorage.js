91a884fe2fa8e690e96903c20e8722313d7f35f2
Riza Fahmi <rizafahmi@gmail.com>
2017-07-21 09:26:49 +0700

    Remove load more button

--- scripts/clientStorage.js
+++ scripts/clientStorage.js
@@ -1,7 +1,5 @@
 /* global define localforage */
 define([], () => {
-  const limit = 4
-  let lastItemId = null
   const videosInstance = localforage.createInstance({
     name: 'videos'
   })
@@ -14,29 +12,16 @@ define([], () => {
   const getVideos = () => {
     return new Promise((resolve, reject) => {
       videosInstance.keys().then(keys => {
-        let index = keys.indexOf(lastItemId)
-        if (index === -1) {
-          index = keys.length
-        }
-        if (index === 0) {
-          return resolve([])
-        }
-        const keySplice = keys.splice(index - limit, limit)
-        videosInstance.getItems(keySplice).then(results => {
+        videosInstance.getItems().then(results => {
           const returnArr = Object.keys(results).map(k => results[k]).reverse()
-          lastItemId = returnArr[returnArr.length - 1].id
           resolve(returnArr)
         })
       })
     })
   }
-  const getLastVideoId = () => {
-    return lastItemId
-  }
 
   return {
     addVideos,
-    getVideos,
-    getLastVideoId
+    getVideos
   }
 })
