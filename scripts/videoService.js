58b1cd970285f4875fad230cb0e1a39d7017829d
Riza Fahmi <rizafahmi@gmail.com>
2017-07-23 23:35:54 +0700

    Offline mode

--- scripts/videoService.js
+++ scripts/videoService.js
@@ -2,20 +2,40 @@
 define(['./template.js', './clientStorage.js'], (template, clientStorage) => {
   const apiUrlPath = 'http://localhost:3000/'
   const apiUrlVideosPath = apiUrlPath + 'videos'
+  const lastVideoId = clientStorage.getLastVideoId()
+  let url
+  if (lastVideoId === null) {
+    url = apiUrlVideosPath + `?_sort=key&_order=desc&_limit=4`
+  } else {
+    url = apiUrlVideosPath +
+      `?_sort=key&_order=desc&key_lte=${lastVideoId + 1}&_limit=4`
+  }
 
   const loadData = () => {
-    const lastVideoId = clientStorage.getLastVideoId()
-    let url
-    if (lastVideoId === null) {
-      url = apiUrlVideosPath + `?_sort=key&_order=desc&_limit=4`
-    } else {
-      url = apiUrlVideosPath +
-        `?_sort=key&_order=desc&key_lte=${lastVideoId + 1}&_limit=4`
-    }
-    fetch(url).then(res => res.json()).then(data => {
-      clientStorage.addVideos(data).then(() => {
-        loadMore()
-      })
+    fetchPromise().then(status => {
+      document.getElementById('connection-status').innerHTML = status
+      loadMore()
+    })
+  }
+
+  const fetchPromise = () => {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => {
+          clientStorage.addVideos(data).then(() => {
+            resolve('The connection is OK, showing latest results')
+          })
+        })
+        .catch(e => {
+          resolve('No connection, showing offline results')
+        })
+      setTimeout(
+        () => {
+          resolve('The connection is hanging, showing offline results')
+        },
+        3000
+      )
     })
   }
 
