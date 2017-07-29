e4aadc47a17697ce6925ab9a0829ad0b41929a0c
Riza Fahmi <rizafahmi@gmail.com>
2017-07-29 21:46:56 +0700

    ServiceWorker Activated event

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
+/* global self, caches */
+
 const cacheName = 'pwa'
 const filesToCache = []
 
@@ -9,4 +11,21 @@ self.addEventListener('install', e => {
       return cache.addAll(filesToCache)
     })
   )
+})
+
+self.addEventListener('activate', e => {
+  console.log('[ServiceWorker] Activated')
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== cacheName) {
+            console.log('[ServiceWorker] Removing old cache', key)
+            return caches.delete(key)
+          }
+        })
+      )
+    })
+  )
+  self.clients.claim()
 })
