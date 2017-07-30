52361be71179fabbb34bc6f8b01694ad0a8261bf
Riza Fahmi <rizafahmi@gmail.com>
2017-07-30 13:21:50 +0700

    Manifest and icon WIP

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,6 @@
 /* global fetch, caches */
-'use strict'
-
-const generateCard = video => {
-  let template = document.querySelector('#card').innerHTML
+function generateCard (video) {
+  var template = document.querySelector('#card').innerHTML
   template = template.replace('{{title}}', video.title)
   template = template.replace('{{description}}', video.description)
   template = template.replace('{{image}}', video.thumbnail)
@@ -10,12 +8,12 @@ const generateCard = video => {
   return template
 }
 
-const appendData = videos => {
-  let videoHTML = ''
-  document.querySelectorAll('.cards__item').forEach(item => {
+function appendData (videos) {
+  var videoHTML = ''
+  document.querySelectorAll('.cards__item').forEach(function (item) {
     item.parentNode.removeChild(item)
   })
-  for (let i = 0; i < videos.length; i++) {
+  for (var i = 0; i < videos.length; i++) {
     videoHTML += generateCard(videos[i])
   }
   document.querySelector('.cards').insertAdjacentHTML('beforeend', videoHTML)
@@ -23,21 +21,23 @@ const appendData = videos => {
 }
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js').then(() => {
+  navigator.serviceWorker.register('./service-worker.js').then(function () {
     console.log('Service Worker Registered')
   })
 }
 
 // Caching strategy
 
-let networkDataReceived = false
+var networkDataReceived = false
 
 const url = 'http://localhost:3000/videos'
 
 // Fetch the last data
 const networkUpdate = fetch(url)
-  .then(response => response.json())
-  .then(json => {
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function (json) {
     networkDataReceived = true
     appendData(json)
   })
@@ -46,21 +46,20 @@ const networkUpdate = fetch(url)
 if ('caches' in window) {
   caches
     .match(url)
-    .then(response => {
+    .then(function (response) {
       if (response) {
         return response.json()
       }
     })
-    .then(json => {
+    .then(function (json) {
       if (!networkDataReceived) {
-        console.log(json)
         appendData(json)
       }
     })
-    .catch(() => {
+    .catch(function () {
       return networkUpdate
     })
-    .catch(() => {
+    .catch(function () {
       console.log('Bad connection')
     })
 }
